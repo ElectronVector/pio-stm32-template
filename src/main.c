0786fd9c386879fd9d39a5a797ac63e146cfc172
Matt <matt@electronvector.com>
2021-03-15 19:52:26 -0600

    Clean up some things

--- src/main.c
+++ src/main.c
@@ -22,49 +22,11 @@ int main(void)
   {
     HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
     
-    HAL_Delay(1000);
+    HAL_Delay(500);
   }
 }
 
 void SysTick_Handler(void)
 {
   HAL_IncTick();
-}
-
-void NMI_Handler(void)
-{
-}
-
-void HardFault_Handler(void)
-{
-  while (1) {}
-}
-
-
-void MemManage_Handler(void)
-{
-  while (1) {}
-}
-
-void BusFault_Handler(void)
-{
-  while (1) {}
-}
-
-void UsageFault_Handler(void)
-{
-  while (1) {}
-}
-
-void SVC_Handler(void)
-{
-}
-
-
-void DebugMon_Handler(void)
-{
-}
-
-void PendSV_Handler(void)
-{
 }
